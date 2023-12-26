9d709abcba451c3b9e54eb7b2b25d69cc17be721
GuDahoon <iamkdh1@naver.com>
2023-12-26 16:52:23 +0900

    studying how to use multi reducer

--- react/react-redux-app/src/index.tsx
+++ react/react-redux-app/src/index.tsx
@@ -4,18 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
-import counter from './reducers'
+import rootReducer from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(counter);
+const store = createStore(rootReducer);
 
-root.render(
+store.dispatch({
+  type: "ADD_TODO",
+  text: "USE REDUX"
+})
+console.log("store.getState() : ", store.getState());
+const render = () => root.render(
   <React.StrictMode>
-    <App />
+    <App 
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />
   </React.StrictMode>
 );
+render();
+
+store.subscribe(render);
 
 reportWebVitals();
