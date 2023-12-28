d5dd43768c936c5477389229f502b94806d01b1b
GuDahoon <iamkdh1@naver.com>
2023-12-28 09:19:55 +0900

    todo 추가 삭제 및 완료 여부 추가

--- react/react-recoil-todo-app/src/App.js
+++ react/react-recoil-todo-app/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import "./App.css";
-import TodoItem from "./components/TodoItem";
 import TodoItemCreator from "./components/TodoItemCreator";
 import { todoListState } from "./todoAtoms";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const todoList = useRecoilValue(todoListState);
   console.log(todoList);
+
   return (
     <div className="App">
       <TodoItemCreator />
+      {todoList.map((item) => (
+        <TodoItem key={item.id} item={item} />
+      ))}
     </div>
   );
 }
--- react/react-recoil-todo-app/src/components/TodoItem.js
+++ react/react-recoil-todo-app/src/components/TodoItem.js
@@ -1,11 +1,47 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { todoListState } from "../todoAtoms";
 
 const TodoItem = ({ item }) => {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem === item) ;
+
+  const editItemText = ({target: {value}}) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: value
+    })
+    setTodoList(newList)
+  }
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    })
+    setTodoList(newList);
+  }
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index)
+    setTodoList(newList);
+  }
+
   return (
     <div>
-      <input type="text" value={item.text} />
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input type="checkbox" checked={item.isComplete} onChange={toggleItemCompletion} />
+      <button onClick={deleteItem}>X</button>
     </div>
   );
 };
 
 export default TodoItem;
+
+function replaceItemAtIndex(array, index, newValue) {
+  return [...array.slice(0, index), newValue, ...array.slice(index + 1)]
+};
+
+function removeItemAtIndex(array, index) {
+  return [...array.slice(0, index), ...array.slice[index + 1]]
+}
