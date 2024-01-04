f022e65ed4cac282fb9d41941d8fba0840257f38
Dahoonkk <iamkdh1@naver.com>
2024-01-04 21:57:22 +0900

    로그인, 회원가입 UI 생성하기

--- react/nextjs-app-jungo-market-clone/src/app/auth/register/page.tsx
+++ react/nextjs-app-jungo-market-clone/src/app/auth/register/page.tsx
@@ -2,13 +2,13 @@
 
 import Button from "@/components/Button";
 import Input from "@/components/Input";
-import { signIn } from "next-auth/react";
+import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const {
@@ -17,6 +17,7 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
@@ -25,7 +26,7 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (body) => {
     setIsLoading(true);
     try {
-      const data = signIn("credentials", body);
+      const { data } = await axios.post("/api/register", body);
       console.log(data);
       router.push("/auth/login");
     } catch (e) {
@@ -41,10 +42,18 @@ const LoginPage = () => {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col justify-center gap-4 min-w-[350px]"
       >
-        <h1 className="text-2xl">Login</h1>
+        <h1 className="text-2xl">Register</h1>
         <Input
           id="email"
           label="Email"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id="name"
+          label="Namne"
           disabled={isLoading}
           register={register}
           errors={errors}
@@ -62,12 +71,9 @@ const LoginPage = () => {
         <Button label="Register" />
         <div className="text-center">
           <p className="text-gray-400">
-            Not a member?{"  "}
-            <Link
-              href="/auth/register"
-              className="text-blue-600 hover:underline"
-            >
-              Register
+            Already a member?{"  "}
+            <Link href="/auth/login" className="text-blue-600 hover:underline">
+              Login
             </Link>
           </p>
         </div>
@@ -76,4 +82,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
