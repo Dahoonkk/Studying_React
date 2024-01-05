642bcc426c9f1c23c2436ff2f12503a52cc2a553
GuDahoon <iamkdh1@naver.com>
2024-01-05 16:04:58 +0900

    Favorite Delete Router 생성하기

--- react/nextjs-app-jungo-market-clone/src/app/api/favorites/[productId]/route.ts
+++ react/nextjs-app-jungo-market-clone/src/app/api/favorites/[productId]/route.ts
@@ -6,7 +6,7 @@ interface Params {
   productId?: string;
 }
 
-export default async function POST(
+export async function POST(
   request: Request,
   { params }: { params: Params }
 ) {
@@ -37,3 +37,36 @@ export default async function POST(
 
   return NextResponse.json(user);
 }
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: Params }
+  ) {
+    const currentUser = await getCurrentUser();
+  
+    if (!currentUser) {
+      return NextResponse.error();
+    }
+  
+    const {productId} = params;
+  
+    if(!productId || typeof productId !== 'string') {
+      throw new Error('Invalid ID');
+    }
+  
+    let favoriteIds = [...(currentUser.favoriteIds || [])]
+  
+    favoriteIds = favoriteIds.filter(id => id !== productId);
+  
+    const user = await prisma?.user.update({
+      where: {
+          id: currentUser.id
+      },
+      data: {
+          favoriteIds: favoriteIds
+      }
+    })
+  
+    return NextResponse.json(user);
+  }
+  
