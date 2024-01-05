378b2375e10cacdcdaff3c9fe922acb5c4bac8eb
GuDahoon <iamkdh1@naver.com>
2024-01-05 15:11:13 +0900

    홈페이지 UI 생성하기

--- react/nextjs-app-jungo-market-clone/src/app/(home)/page.tsx
+++ react/nextjs-app-jungo-market-clone/src/app/(home)/page.tsx
@@ -1,18 +1,40 @@
-import getProducts, { ProductsParams } from "../actions/getProducsts"
-
+import Container from "@/components/Container";
+import getProducts, { ProductsParams } from "../actions/getProducsts";
+import EmptyState from "@/components/EmptyState";
+import ProductCard from "@/components/ProductCard";
+import getCurrentUser from "../actions/getCurrentUser";
+import FloatingButton from "@/components/FloatingButton";
 
 interface HomeProps {
   searchParams: ProductsParams;
 }
 
-export default async function Home({searchParams}: HomeProps) {
-
-  const products = await getProducts(searchParams)
-  console.log('@@@@@', products)
+export default async function Home({ searchParams }: HomeProps) {
+  const products = await getProducts(searchParams);
+  // console.log('@@@@@', products)
+  const currentUser = await getCurrentUser();
 
   return (
-    <main>
-      누구나 볼 수 있는 페이지입니다.
-    </main>
-  )
+    <Container>
+      {/* Categories /*/}
+      {products?.data.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <>
+          <div className="grid gid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:gird-cols-4 2xl:gird-cols-6">
+            {products.data.map((product) => (
+              <ProductCard
+                currentUser={currentUser}
+                key={product.id}
+                data={product}
+              />
+            ))}
+          </div>
+        </>
+      )}
+      <FloatingButton 
+        href="/products/upload"
+      >+</FloatingButton>
+    </Container>
+  );
 }
