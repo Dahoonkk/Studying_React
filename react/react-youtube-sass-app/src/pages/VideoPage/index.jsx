793b8fd1af483b145b95ce6caee278d6ded67187
GuDahoon <iamkdh1@naver.com>
2024-01-18 08:55:44 +0900

    VideoPage UI 생성하기

--- react/react-youtube-sass-app/src/pages/VideoPage/index.jsx
+++ react/react-youtube-sass-app/src/pages/VideoPage/index.jsx
@@ -3,38 +3,128 @@ import { useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Youtube from "react-youtube";
 import { SideBarContext } from "../../context/SideBarContext";
+import { BiDislike, BiLike } from "react-icons/bi";
+import { RiFlagLine, RiShareForwardLine } from "react-icons/ri";
+import { MdPlaylistAdd } from "react-icons/md";
+import dayjs from 'dayjs';
 
 const VideoPage = () => {
   const { videoId } = useParams();
   let location = useLocation();
   const { state: currentVideo } = location;
-  const { setIsToggled } = useContext(SideBarContext)
+  const { setIsToggled } = useContext(SideBarContext);
 
   useEffect(() => {
     setIsToggled(false);
-  }, [])
+  }, []);
 
   const onPlayerReady = (e) => {
     e.target.playVideo();
   };
 
   const opts = {
-    height: '390',
-    width: '640',
+    height: "390",
+    width: "640",
     palyerVars: {
       autoplay: 1,
-    }
-  }
+    },
+  };
+
+  const videoHeaderMarkUp = (
+    <div className="video_main_info">
+      <div className="tags">
+        {
+          currentVideo?.snippet?.tags?.map((tag, i) => (
+            <p className='tag' key={i}>#{tag}</p>
+          ))
+        }
+      </div>
+      <h1>{currentVideo.snippet.title}</h1>
+      <div className="videoplayer_metadata">
+        <span>
+          {currentVideo.extraInfo.viewCount} vies
+        </span>
+        <span className="dot_separator"> &#8226; </span>
+        <span>
+          {dayjs(currentVideo.snippet.publishedAt).format('MMM D, YYYY')}
+        </span>
+      </div>
+    </div>
+  )
 
   return (
-    <div>
-      <Youtube
-        className="youtube-player"
-        videoId={videoId}
-        onPlay={onPlayerReady}
-        opts={opts}
-      />
-    </div>
+    <section className="videoPage">
+      <div className="columns_container">
+        <div className="column column_1">
+          <div className="youtube_player_container">
+            <Youtube
+              className="youtube-player"
+              videoId={videoId}
+              onPlay={onPlayerReady}
+              opts={opts}
+            />
+          </div>
+          <div className="videoplayer_info">
+            {/* videoHeaderMarkUp */}
+            {videoHeaderMarkUp}
+            <div className="main_header_buttons">
+              <div className="likes_container">
+                <div className="likes">
+                  <BiLike size={25} />
+                  <span>likes</span>
+                </div>
+                <div className="dislikes">
+                  <BiDislike size={25} />
+                  <span>dislikes</span>
+                </div>
+              </div>
+              <div className="share">
+                <RiShareForwardLine size={25} />
+                <span>SHARE</span>
+              </div>
+              <div className="save">
+                <MdPlaylistAdd size={25} />
+                <span>SAVE</span>
+              </div>
+              <div className="report">
+                <RiFlagLine size={25} className="sidebar_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="channel_video_info">
+            <div className="channel_data">
+              <div className="channel_avatar">
+                <img src={currentVideo.channelInfo.thumbnails.default.url} alt="avatar" />
+              </div>
+              <div className="channel_title">
+                <a href="/">
+                  {currentVideo.channelInfo.title}
+                </a>
+                <span>
+                  {currentVideo.channelInfo.subscriberCount} subscribers
+                </span>
+              </div>
+              <div className="channel_subscribe">
+                <button>
+                  SUBSCRIBED
+                </button>
+              </div>
+            </div>
+            <div className="video_description">
+              {currentVideo.snippet.description}
+            </div>
+          </div>
+          <div className="video_comments_container">
+            <div className="video_comments_count">
+              {currentVideo.extraInfo.commentCount} Comments
+            </div>
+            <div className="video_comments">
+              {/* videoCommentsMarkUp */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
