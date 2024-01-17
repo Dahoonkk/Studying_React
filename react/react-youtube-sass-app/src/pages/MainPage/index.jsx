d4a83c2115a4aedba762a0fea54846b5bea4c91e
GuDahoon <iamkdh1@naver.com>
2024-01-17 14:44:02 +0900

    메인 페이지 UI 생성 및 스타일링

--- react/react-youtube-sass-app/src/pages/MainPage/index.jsx
+++ react/react-youtube-sass-app/src/pages/MainPage/index.jsx
@@ -1,25 +1,25 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "../../api/axios";
 import { getVideoInfo } from "../../context/fetchingData";
+import VideoCard from "../../components/VideoCard";
 
 const MainPage = () => {
-  const storedVideos = JSON.parse(localStorage.getItem('mainVideos'));
+  const storedVideos = JSON.parse(localStorage.getItem("mainVideos"));
   const [mainVideos, setMainVideos] = useState(storedVideos || []);
 
   const getMainVideos = useCallback(async () => {
     try {
-      if(!storedVideos) {
-
+      if (!storedVideos) {
         const response = await axios.get(
-          '/search?part=snippet&maxResults=10&q=beautiful%20place'
-          );
-          let videosArray = await response.data.items;
-          // console.log('videosArray1', videosArray);
-          videosArray = await getVideoInfo(videosArray)
-          // console.log('videosArray2', videosArray);
-          setMainVideos(videosArray)
-          localStorage.setItem('mainVideos', JSON.stringify(videosArray));
-        }
+          "/search?part=snippet&maxResults=10&q=beautiful%20place"
+        );
+        let videosArray = await response.data.items;
+        // console.log('videosArray1', videosArray);
+        videosArray = await getVideoInfo(videosArray);
+        // console.log('videosArray2', videosArray);
+        setMainVideos(videosArray);
+        localStorage.setItem("mainVideos", JSON.stringify(videosArray));
+      }
     } catch (error) {
       console.log(error);
     }
@@ -30,8 +30,20 @@ const MainPage = () => {
   }, [getMainVideos]);
 
   return (
-    <div>MainPage</div>
-  )
+    <section className="mainGallery">
+      {mainVideos.map((video) => (
+        <VideoCard
+          key={video.id.videoId}
+          id={video.id.videoId}
+          video={video}
+          img={video.snippet.thumbnails.medium.url}
+          info={video.snippet}
+          eInfo={video.extraInfo}
+          channelInfo={video.channelInfo}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default MainPage;
