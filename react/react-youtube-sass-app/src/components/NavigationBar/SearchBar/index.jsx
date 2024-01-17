9bf0331dd959d571cb21efd98441cc8653bff477
GuDahoon <iamkdh1@naver.com>
2024-01-17 09:10:10 +0900

    오류 수정

--- react/react-youtube-sass-app/src/components/NavigationBar/SearchBar/index.jsx
+++ react/react-youtube-sass-app/src/components/NavigationBar/SearchBar/index.jsx
@@ -1,31 +1,43 @@
-import { ImSearch } from "react-icons/im";
-import { MdKeyboardVoice } from "react-icons/md";
-import useWindowSize from "../../../helpers/useWindowSize";
+/* eslint-disable react/prop-types */
 import { useContext } from "react";
+import { ImSearch as SearchIcon } from "react-icons/im";
+import { MdKeyboardVoice as VoiceIcon } from "react-icons/md";
 import { SearchContext } from "../../../context/SearchContext";
+import useWindowSize from "../../../helpers/useWindowSize";
 
 const SearchBar = () => {
   const { width } = useWindowSize();
-  const {setShowSpecialSearchBar} = useContext(SearchContext)
+
+  const {setShowSpecialSearchBar } = useContext(SearchContext);
 
   return (
-    <div className={`SearchBar ${width <= 640 ? "smallSearch" : ""}`}>
+    <div className={`SearchBar  ${width <= 640 ? "smallSearch" : ""}`}>
       {width > 640 ? (
-        <form >
-          <input type="text" name="search" placeholder="Search" autoComplete="false"/>
+        <form>
+          <input
+            type="text"
+            name="search"
+            placeholder="Search"
+          />
           <button type="submit">
-            <ImSearch size={20} data-tip="Search" data-for="navbar" />
+            <SearchIcon size={20} data-tip="Search" data-for="navbar" />
           </button>
         </form>
       ) : (
-        <button className="icon-container searchIcon"
+        <button
+          className="icon-container searchIcon"
           onClick={() => setShowSpecialSearchBar(true)}
         >
-          <ImSearch size={20} />
+          <SearchIcon size={20} data-tip="Search" data-for="navbar" />
         </button>
       )}
+
       <button className="icon-container voiceIcon">
-        <MdKeyboardVoice size={25} />
+        <VoiceIcon
+          size={25}
+          data-tip="Search with your voice"
+          data-for="navbar"
+        />
       </button>
     </div>
   );
--- react/react-youtube-sass-app/src/helpers/useWindowSize.jsx
+++ react/react-youtube-sass-app/src/helpers/useWindowSize.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react'
 
 const useWindowSize = () => {
+
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
   });
 
   useEffect(() => {
-    const handleResize = () => {
-        setWindowSize({
-            width: windowSize.innerWidth,
-            height: windowSize.innerHeight,
-        });
+    const handleResize =() => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     }
     window.addEventListener("resize", handleResize);
     handleResize();
-
+    
     return () => {
-        window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize)
     }
-  }, [])
+  }, []);
 
-  return windowSize;
-};
+  return windowSize
+}
 
-export default useWindowSize;
+export default useWindowSize
