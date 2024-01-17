e0983b9899cc14f2889c753b77b113b11115a3de
GuDahoon <iamkdh1@naver.com>
2024-01-17 12:02:06 +0900

    SmallSideBar 생성 및 스타일링

--- react/react-youtube-sass-app/src/components/SideBar/SmallSideBar.jsx
+++ react/react-youtube-sass-app/src/components/SideBar/SmallSideBar.jsx
@@ -1,9 +1,43 @@
-
+import { AiFillHome } from "react-icons/ai";
+import { FaCompass } from "react-icons/fa";
+import { MdSubscriptions } from "react-icons/md";
+import { Link } from "react-router-dom";
+import "react-tooltip/dist/react-tooltip.css"; //반드시 필요
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 const SmallSideBar = () => {
   return (
-    <div style={{ color: 'white', marginTop: 200, fontSize: 200}}>SmallSideBar</div>
-  )
-}
+    <aside className="small_sidebar">
+      <div className="small_sidebar_container">
+        <Link to="/" tooltip-data-id="sidebar" tooltip-data-content="Home">
+          <div className="sidebar_icon_container">
+            <AiFillHome size={23} className="sidebar_icon" />
+            <h4 className="sidebar_text">Home</h4>
+          </div>
+        </Link>
+        <Link to="/" tooltip-data-id="sidebar" tooltip-data-content="Explore">
+          <div className="sidebar_icon_container">
+            <FaCompass size={23} className="sidebar_icon" />
+            <h4 className="sidebar_text">Explore</h4>
+          </div>
+        </Link>
+        <Link
+          to="/"
+          tooltip-data-id="sidebar"
+          tooltip-data-content="Subscriptions"
+        >
+          <div className="sidebar_icon_container">
+            <MdSubscriptions size={23} className="sidebar_icon" />
+            <h4 className="sidebar_text">Subscriptions</h4>
+          </div>
+        </Link>
+      </div>
+      <ReactTooltip 
+        id="sidebar"
+        place="bottom"
+      />
+    </aside>
+  );
+};
 
-export default SmallSideBar
+export default SmallSideBar;
