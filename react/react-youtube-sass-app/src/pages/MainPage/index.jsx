bdc9f52a7abd5b98af01f250d28fc0a95a151b7a
GuDahoon <iamkdh1@naver.com>
2024-01-17 14:20:06 +0900

    비디오 데이터 가져오기

--- react/react-youtube-sass-app/src/pages/MainPage/index.jsx
+++ react/react-youtube-sass-app/src/pages/MainPage/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useCallback, useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { getVideoInfo } from "../../context/fetchingData";
 
 const MainPage = () => {
+  const [mainVideos, setMainVideos] = useState([]);
+
+  const getMainVideos = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `/search?part=snippet&maxResults=10&q=beautiful%20place`
+      );
+      let videosArray = await response.data.items;
+      console.log('videosArray1', videosArray);
+      videosArray = await getVideoInfo(videosArray)
+      console.log('videosArray2', videosArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getMainVideos();
+  }, [getMainVideos]);
+
   return (
     <div>MainPage</div>
   )
-}
+};
 
-export default MainPage
+export default MainPage;
