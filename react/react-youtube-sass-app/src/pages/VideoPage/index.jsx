f7c7eeb204c6c543822143fdfc76828c827bf77c
GuDahoon <iamkdh1@naver.com>
2024-01-17 15:52:50 +0900

    VideoPage 데이터 가져오기

--- react/react-youtube-sass-app/src/pages/VideoPage/index.jsx
+++ react/react-youtube-sass-app/src/pages/VideoPage/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import Youtube from "react-youtube";
 
 const VideoPage = () => {
-  return (
-    <div>VideoPage</div>
-  )
-}
+  const { videoId } = useParams();
+  let location = useLocation();
+  const { state: currentVideo } = location;
 
-export default VideoPage
+  const onPlayerReady = (e) => {
+    e.target.playVideo();
+  };
+
+  const opts = {
+    height: '390',
+    width: '640',
+    palyerVars: {
+      autoplay: 1,
+    }
+  }
+
+  return (
+    <div>
+      <Youtube
+        className="youtube-player"
+        videoId={videoId}
+        onPlay={onPlayerReady}
+        opts={opts}
+      />
+    </div>
+  );
+};
+
+export default VideoPage;
