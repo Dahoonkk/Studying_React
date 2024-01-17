37a76cde43ee62490df4049c7cbf998fe6febec6
GuDahoon <iamkdh1@naver.com>
2024-01-17 14:31:30 +0900

    API 제한으로 인해 가져온 비디오 데이터 로컬 storage에 저장하기

--- react/react-youtube-sass-app/src/pages/MainPage/index.jsx
+++ react/react-youtube-sass-app/src/pages/MainPage/index.jsx
@@ -3,21 +3,27 @@ import axios from "../../api/axios";
 import { getVideoInfo } from "../../context/fetchingData";
 
 const MainPage = () => {
-  const [mainVideos, setMainVideos] = useState([]);
+  const storedVideos = JSON.parse(localStorage.getItem('mainVideos'));
+  const [mainVideos, setMainVideos] = useState(storedVideos || []);
 
   const getMainVideos = useCallback(async () => {
     try {
-      const response = await axios.get(
-        `/search?part=snippet&maxResults=10&q=beautiful%20place`
-      );
-      let videosArray = await response.data.items;
-      console.log('videosArray1', videosArray);
-      videosArray = await getVideoInfo(videosArray)
-      console.log('videosArray2', videosArray);
+      if(!storedVideos) {
+
+        const response = await axios.get(
+          '/search?part=snippet&maxResults=10&q=beautiful%20place'
+          );
+          let videosArray = await response.data.items;
+          // console.log('videosArray1', videosArray);
+          videosArray = await getVideoInfo(videosArray)
+          // console.log('videosArray2', videosArray);
+          setMainVideos(videosArray)
+          localStorage.setItem('mainVideos', JSON.stringify(videosArray));
+        }
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [storedVideos]);
 
   useEffect(() => {
     getMainVideos();
