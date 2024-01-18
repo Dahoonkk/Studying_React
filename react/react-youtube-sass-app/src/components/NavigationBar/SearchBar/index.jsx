f81734bd529632d01f84714b3599bb3a9c24cfef
GuDahoon <iamkdh1@naver.com>
2024-01-18 10:41:45 +0900

    SearchBar 검색 기능 추가

--- react/react-youtube-sass-app/src/components/NavigationBar/SearchBar/index.jsx
+++ react/react-youtube-sass-app/src/components/NavigationBar/SearchBar/index.jsx
@@ -4,17 +4,45 @@ import { ImSearch as SearchIcon } from "react-icons/im";
 import { MdKeyboardVoice as VoiceIcon } from "react-icons/md";
 import { SearchContext } from "../../../context/SearchContext";
 import useWindowSize from "../../../helpers/useWindowSize";
+import axios from "../../../api/axios";
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
   const { width } = useWindowSize();
 
-  const {setShowSpecialSearchBar } = useContext(SearchContext);
+  const navigate = useNavigate();
+
+  const { searchQuery, setSearchQuery, setShowSpecialSearchBar } =
+    useContext(SearchContext);
+
+  const handleChange = (e) => {
+    setSearchQuery({
+      ...searchQuery,
+      input: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(searchQuery.input !== '') {
+      const response = await axios.get(`/search?part=snippet&maxResults=10&q=${searchQuery.input}`)
+
+      setSearchQuery({
+        ...searchQuery,
+        videos: response.data.items
+      })
+
+      navigate(`/results/${searchQuery.input}`)
+    }
+  }
 
   return (
     <div className={`SearchBar  ${width <= 640 ? "smallSearch" : ""}`}>
       {width > 640 ? (
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
+            value={searchQuery.input}
+            onChange={handleChange}
             type="text"
             name="search"
             placeholder="Search"
